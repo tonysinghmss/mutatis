4062ae943470428f25cda2133498412459a5aac9
tonysinghmss <manishankarsngh007@gmail.com>
2019-07-27 00:14:53 +0530

    Class modified according to attrs

--- mutandis/emissary.py
+++ mutandis/emissary.py
@@ -1,14 +1,24 @@
 """
 emissary.py is used for storing the details like struct, typedef, union, variables, macros, etc.
 """
+import attr
+
+@attr.s
 class Emissary(object):
     """ Collection of all messages. """
-    def __init__(self):
-        #TODO : Set all parameters.
-        pass
+    pass
 
+@attr.s
 class Message(object):
-    """ Base class which will be inherited by other message classes of this module."""
-    def __init__(self):
-        pass
+    """ Base class which will be inherited by other message classes of this module."""    
+    address=attr.ib(default=None) # A tuple of(file_name, lineno)
+    permitted_include_rule_vals = ["always", "never", "if_needed"]
+    include_rule=attr.ib(default="always", validator=attr.validators.in_(permitted_include_rule_vals))
+    errors=attr.ib(factory=list)
+    warnings=attr.ib(factory=list)
+    
 
+@attr.s
+class StructMessage(Message):
+    """Class to store struct and its details"""
+    pass
