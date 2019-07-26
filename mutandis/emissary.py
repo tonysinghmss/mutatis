eaf8e99566fcea0dcdf8e34d50ba2f604bcceae9
Manishankar Singh <manishankarsngh007@gmail.com>
2019-07-27 00:35:21 +0530

    Attribute name refactored

--- mutandis/emissary.py
+++ mutandis/emissary.py
@@ -12,8 +12,8 @@ class Emissary(object):
 class Message(object):
     """ Base class which will be inherited by other message classes of this module."""    
     address=attr.ib(default=None) # A tuple of(file_name, lineno)
-    permitted_include_rule_vals = ["always", "never", "if_needed"]
-    include_rule=attr.ib(default="always", validator=attr.validators.in_(permitted_include_rule_vals))
+    permitted_inclusion_vals = ["always", "never", "if_needed"]
+    inclusion_policy=attr.ib(default="always", validator=attr.validators.in_(permitted_inclusion_vals))
     errors=attr.ib(factory=list)
     warnings=attr.ib(factory=list)
     
